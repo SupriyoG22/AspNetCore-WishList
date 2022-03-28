c629672a63a856ec9cfeaffb1f118a5c56025ec5
vmadmin <vmadmin@vssql>
2022-03-28 08:51:44 +0000

    Code Changes

---
+++ WishList/Controller/ItemController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WishList.Data;
+using WishList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WishList
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_context.Items.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == Id);
+            if(item != null)
+            {
+                _context.Items.Remove(item);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
