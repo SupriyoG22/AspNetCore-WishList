d7ededdfa075ff1abe5fd065d71efe6f702dc746
vmadmin <vmadmin@vssql>
2022-03-30 06:50:46 +0000

    Changes

--- WishListTests/AddMVCMiddlewareTests.cs
+++ WishListTests/AddMVCMiddlewareTests.cs
@@ -32,22 +32,21 @@ namespace WishListTests
             Assert.True(file.Contains("app.UseRouting();"), "`Startup.cs`'s `Configure` method did not contain a call to `UseRouting` on `app`.");
         }
 
-        //[Fact(DisplayName = "Configure MVC Middleware In Configure @configure-useendpoints-middleware-in-configure")]
-        //[Fact(Skip = "Doesn't work at the moment")]
-        //public void UseEndpointsAdded()
-        //{
-        //    var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Startup.cs";
-        //    string file;
-        //    using (var streamReader = new StreamReader(filePath))
-        //    {
-        //        file = streamReader.ReadToEnd();
-        //    }
+        [Fact(DisplayName = "Configure MVC Middleware In Configure @configure-useendpoints-middleware-in-configure")]
+        public void UseEndpointsAdded()
+        {
+            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "WishList" + Path.DirectorySeparatorChar + "Startup.cs";
+            string file;
+            using (var streamReader = new StreamReader(filePath))
+            {
+                file = streamReader.ReadToEnd();
+            }
 
-        //    Assert.True(file.Contains("app.UseRouting();"), "`Startup.cs`'s `Configure` method did not contain a call to `UseRouting` on `app`.");
+            Assert.True(file.Contains("app.UseRouting();"), "`Startup.cs`'s `Configure` method did not contain a call to `UseRouting` on `app`.");
 
-        //    var pattern = @"app.UseRouting\s*?[(]\s*?[)]\s*?;\s*?app.UseEndpoints\s*?[(]\s*?endpoints\s*?=>\s*?{\s*?endpoints.MapDefaultControllerRoute\s*?[(]\s*?[)]\s*?;\s*?}\s*?[)]\s*?;";
-        //    var rgx = new Regex(pattern);
-        //    Assert.True(rgx.IsMatch(file), "`Startup.cs`'s `Configure` method did not contain a call to `UseEnpoints` on `app` after `UseRouting` with an argument of `endpoints => { endpoints.MapDefaultControllerRoute(); }`");
-        //}
+            var pattern = @"app.UseRouting\s*?[(]\s*?[)]\s*?;\s*?app.UseEndpoints\s*?[(]\s*?endpoints\s*?=>\s*?{\s*?endpoints.MapDefaultControllerRoute\s*?[(]\s*?[)]\s*?;\s*?}\s*?[)]\s*?;";
+            var rgx = new Regex(pattern);
+            Assert.True(rgx.IsMatch(file), "`Startup.cs`'s `Configure` method did not contain a call to `UseEnpoints` on `app` after `UseRouting` with an argument of `endpoints => { endpoints.MapDefaultControllerRoute(); }`");
+        }
     }
 }
